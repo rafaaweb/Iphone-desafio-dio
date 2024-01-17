8b68f1f64cc3ec7ec6e29a413e21c262e12602af
rafaaweb <rafa.ads@outlook.com>
2024-01-17 19:03:35 -0300

    Desafio do Iphone Dio

---
+++ Iphone-desafio-dio/src/main/java/Iphone.java
@@ -0,0 +1,50 @@
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, NavegadorInternet {
+
+    String cor;
+    String capacidade;
+
+    @Override
+    public void ligar(String numero) {
+        //Implementacao
+    }
+
+    @Override
+    public void atender(String numero) {
+        //Implementacao
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //Implementacao
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //Implementacao
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        //Implementacao
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //Implementacao
+    }
+
+    @Override
+    public void tocar() {
+        //Implementacao
+    }
+
+    @Override
+    public void pausar() {
+        //Implementacao
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //Implementacao
+    }
+}
---
+++ Iphone-desafio-dio/src/main/java/NavegadorInternet.java
@@ -0,0 +1,9 @@
+public interface NavegadorInternet {
+
+    public void exibirPagina(String url);
+
+    public void adicionarNovaAba(String url);
+
+    public void atualizarPagina();
+
+}
